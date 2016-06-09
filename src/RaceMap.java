c5ec27946ac1ad447b5e1ad25e7514dc3f2298d9
Carnitter <rottor467@gmail.com>
2016-06-09 10:32:57 +0200

    sound player
    nieuwe anum class met mogelijkhijd om geluiden af te spelen

--- src/RaceMap.java
+++ src/RaceMap.java
@@ -112,6 +112,7 @@ public class RaceMap extends JPanel implements WiimoteListener, ActionListener {
 			wiimote = wiimotes[i];
 			wiimote.activateMotionSensing();
 			wiimote.addWiiMoteEventListeners(this);
+			SoundEffect.MUSIC.play();
 		}
 
 		values = new ArrayList<>();
